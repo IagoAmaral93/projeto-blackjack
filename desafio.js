832b3385acb3972527d1722a5d1fd2ebac91c563
iagoamaral93 <iagoamaral1993@gmail.com>
2023-03-23 22:33:43 -0300

    Projeto finalizado

--- desafio.js
+++ desafio.js
@@ -9,5 +9,82 @@
  * 
  * 
  * 
- */
+ **/
+// function comecoDoJogo(){
+//    if (confirm(`Boas Vindas ao jogo de BlackJack! \nQuer iniciar uma nova rodada ?`)=== true){
+//       let cartasUsuario = [comprarCarta(), comprarCarta()]
+//       let cartasPc = []
+//       let resultadoUsuario
+//       let cartasPc1 = comprarCarta()
+//       let cartasPc2 = comprarCarta()
+//       cartasPc.push(cartasPc1.texto)
+//       cartasPc.push(cartasPc2.texto)
+//       let cartasCompradas = ``
+//       let calculoUsuario = calculoUsuario[0].valor + cartasUsuario[1].valor
+//       let calculoPc = cartasPc1.valor + cartasPc2.valor
 
+//       let compra = confirm(`Cartas do Usuario - ${cartasUsuario[0].texto} e ${cartasUsuario[1].texto}`)
+//    }
+// }
+
+
+
+
+
+function comecoDoJogo() {
+   if (confirm("Boas vindas ao jogo de BlackJack!\nQuer iniciar uma nova rodada?") === true) {
+       let cartasUsuario = [comprarCarta(), comprarCarta()]
+       let cartasPc = []
+       let resultadoUsuario = []
+       //cartasUsuario.push(cartasUsuario1.texto)
+       //cartasUsuario.push(cartasUsuario2.texto)
+       let cartasPc1 = comprarCarta()
+       let cartasPc2 = comprarCarta()
+       cartasPc.push(cartasPc1.texto)
+       cartasPc.push(cartasPc2.texto)
+let cartasCompradas = 0
+       let calculoUsuario = cartasUsuario[0].valor + cartasUsuario[1].valor
+       let calculoPc = cartasPc1.valor + cartasPc2.valor
+
+       let compra = confirm(`Cartas do Usuário - ${cartasUsuario[0].texto} e ${cartasUsuario[1].texto} - ${calculoUsuario} \nCartas do Computador - ${cartasPc1.texto}\nDeseja comprar mais cartas ?`)
+
+       let i = 0
+       while (compra) {
+           let novaCarta = comprarCarta()
+           cartasUsuario.push(novaCarta)
+calculoUsuario = 0
+           for (let carta of cartasUsuario) {
+
+               calculoUsuario += carta.valor
+               resultadoUsuario.push(carta.texto)
+           }
+               
+           
+           if (calculoUsuario > 21) {
+               break
+           }
+           
+           for (let carta of resultadoUsuario) {
+                   confirm(`Cartas do Usuário - ${carta.split(' e ')} e - total: ${calculoUsuario} \nCartas do Computador - ${cartasPc1.texto}\nDeseja co
+mprar mais cartas ?`)
+               }
+
+           //console.log(`Nova Carta = ${cartasUsuario[0]}`)
+           i ++
+       }
+       console.log(`Cartas do Usuário - ${cartasUsuario[0].texto} e ${cartasUsuario[1].texto} - ${calculoUsuario}`)
+       console.log(`Cartas do Computador - ${cartasPc1.texto} e ${cartasPc2.texto} - ${calculoPc}`)
+       if (calculoUsuario === 2 * 'A') {
+           confirm('Fim de jogo')
+           console.log('Fim de jogo')
+       } else if (calculoUsuario > 21) {
+         confirm('Computador ganhou , usuário estourou!')
+         console.log('Computador ganhou , usuário estourou! ')
+     } else if (calculoUsuario > calculoPc) {
+         confirm('Usuário ganhou !')
+         console.log('Usuário ganhou ! ')
+     } else if (calculoUsuario < calculoPc) {
+         confirm('Computador ganhou !')
+         console.log('Computador ganhou ! ')
+     } else if (calculoUsuario === calculoPc) {
+         confirm('EMPATE')}
